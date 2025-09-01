3962944d7def797ddc0e337623e6f87e8e050aed
Elleined <demadegu@gmail.com>
2025-09-01 13:50:19 +0800

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM eclipse-temurin:21-jdk-alpine
+RUN apk add --no-cache tzdata && \
+    ln -snf /usr/share/zoneinfo/Asia/Manila /etc/localtime && \
+    echo "Asia/Manila" > /etc/timezone
+WORKDIR /app
+COPY build/libs/spring-boot-social-login.jar ./spring-boot-social-login.jar
+ENTRYPOINT ["java", "-jar", "spring-boot-social-login.jar"]
---
+++ src/main/java/com/elleined/spring_boot_social_login/SpringBootSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.elleined.spring_boot_social_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSocialLoginApplication.class, args);
+	}
+
+}
